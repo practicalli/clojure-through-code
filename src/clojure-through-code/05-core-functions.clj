baa5eaccfa3d615389a95a7dc637998628e190bb
John Stevenson <john@jr0cket.co.uk>
2015-04-01 23:31:02 +0100

    Initial examples of Clojure.core functions
    map, take, first

--- src/clojure-through-code/05-core-functions.clj
+++ src/clojure-through-code/05-core-functions.clj
@@ -24,3 +24,53 @@
 
 ;; these are the building blocks for your own code and other Clojure libraries
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Working with data structures
+
+;; This section will become far too long, so need to refine the topic
+
+;; Apply a function over the individual elements a data structure
+
+
+
+
+
+;; Mapping a function over one or more collections, applying that function to the element of each collection
+
+;; user> (doc map)
+;; -------------------------
+;; clojure.core/map
+;; ([f coll] [f c1 c2] [f c1 c2 c3] [f c1 c2 c3 & colls])
+;;   Returns a lazy sequence consisting of the result of applying f to the set of first items of each coll, followed by applying f to the set of second items in each coll, until any one of the colls is exhausted.  Any remaining items in other colls are ignored. Function f should accept number-of-colls arguments.
+;; nil
+
+
+
+(map + [1 2 3] [1 2 3])
+
+(map + [1 2 3] [1 2])
+
+(map + [1 2 3] [1])
+
+(map + [1 2 3] [])
+
+(map + [1 2 3])
+
+
+(def fibonacci-sequence [1 2 3 5 8 13 21 34 55 89 144 278])
+
+(take 20 fibonacci-sequence)
+
+(take 10 [])
+
+(first [1 2 3])
+(first '())
+(first (list))
+
+(vector(map * (range 10) fibonacci-sequence))
+;; return a vector for this
+
+(lazy-seq)
+
+(rand-int 10)
