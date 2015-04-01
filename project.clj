11f49599c3b7c109e02b13b093c7f9c4af62df5f
John Stevenson <john@jr0cket.co.uk>
2015-04-01 23:38:46 +0100

    Bumped version of project to 20.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clojure-through-code "20.1.4-SNAPSHOT"
+(defproject clojure-through-code "20.1.5-SNAPSHOT"
   :description "Learning Clojure by evaluating code on the fly"
   :url "http://jr0cket.co.uk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
+
