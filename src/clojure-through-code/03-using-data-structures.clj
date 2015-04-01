eeb7689a5c29131d47c160becb8bb1a28a10cdf5
John Stevenson <john@jr0cket.co.uk>
2015-04-01 23:04:48 +0100

    Adding more examples of defing data structures

--- src/clojure-through-code/03-using-data-structures.clj
+++ src/clojure-through-code/03-using-data-structures.clj
@@ -6,18 +6,108 @@
 
 ;; Defining things is easy in Clojure
 
+;; Defining things is as simple as giving a name to a value, of course in Clojure the evaluation of a function is also a value.
+
 (def person "Jane Doe")
 
-(def my-data [1, 2, 3, "frog" person])
+;; Names are of course case sensitive, so Person is not the same as person
+(def Person "James Doh")
 
-(def developer-events
+;; Clojure uses dynamic typing, this means its trivial to mix and match different kinds of data.
+;; Here we are defining a name for a vector, which contains numbers, a string and name of another def. 
+(def my-data [1 2 3 "frog" person])
+
+my-data
+
+;; You can also dynamically re-define a name to points to a different value
+(def my-data [1 2 3 4 5 "frog" person])
+
+;; the original value that defined my-data remains unchanged (its immutable), so anything using that value remains unaffected.  Essentially we are re-mapping my-data to a new value.
+
+
+;; Lets define a name to point to a list of numbers
+(def my-list '(1 2 3))
+
+;; We are returned that list of numbers when we evaluate the name
+
+my-list
+
+;; We can use the cons function to add a number to our list,
+;; however because lists are immutable, rather than changing the original list, a new one is returned
+;; So if we want to keep on refering to our "changed" list, we need to give it a name
+(def my-list-updated (cons 4 my-list))
+
+;; As you can see we have not changed the original list
+my-list
+
+;; The new list does have the change though.
+my-list-updated
+
+;; As names in Clojure are dynamic, we can of course point the original name to the new list
+(def my-list (cons 5 my-list))
+
+;; so now when we evaluate the original name, we get the updated list 
+my-list
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Practising with lists
+
+
+;; Create a simple collection of developer event names
+
+;; first lets use a list of strings at it seems the easiest straight forward
+
+(def developer-events-strings '("Devoxx UK" "Devoxx France" "Devoxx" "Hack the Tower"))
+
+;; > Remember, in Clojure the first element of a list is treated as a function call, so we have used the quote ' character to tell Cloojure to also treat the first element as data.  We could of course have used the list function to define our list `(def developer-events-strings2 (list "Devoxx UK" "Devoxx France" "Devoxx" "Hack the Tower"))`
+
+developer-events-strings
+
+;; We can get just the first element in our collection of developer events
+(first developer-events-strings)
+
+
+;; using a Clojure Vector data structure seems a little more Clojurey, especially when the vector contains keywords.  Think of a Vector as an Array, although in Clojure it is again immutable in the same way a list is.
+
+(def developer-events-vector
   [:devoxxuk :devoxxfr :devoxx :hackthetower] )
 
-(def dev-event-details
-  {:devoxxuk       {:URL "http://devoxx.co.uk"}
-   :hackthetower {:URL "http://hackthetower.co.uk"}})
 
-(def portfolio [ { :ticker "AAPL" :lastTrade 203.25 :open 204.50}
+;; Lets create a slightly more involved data structure,
+;; holding more data around each developer events.
+;; This data structure is just a map, with each key being
+;; the unique name of the developer event.
+;; The details of each event (the value to go with the
+;; event name key) is itself a map as there are several
+;; pieces of data associated with each event name.
+;; So we have a map where each value is itself a map.
+(def dev-event-details
+  {:devoxxuk     {:URL "http://jaxlondon.co.uk"
+                  :event-type "Conference"
+                  :number-of-attendees 700
+                  :call-for-papers true}
+   :hackthetower {:URL "http://hackthetower.co.uk"
+                  :event-type "hackday"
+                  :number-of-attendees 60
+                  :call-for-papers false}})
+
+;; lets call the data structre and see what it evaluates too, it should not be a surprise
+dev-event-details
+
+;; We can ask for the value of a specific key, and just that value is returned
+(dev-event-details :devoxxuk)
+
+;; In our example, the value returned from the :devoxxuk key is also a map,
+;; so we can ask for a specific part of that map value by again using its key
+(:URL (dev-event-details :devoxxuk))
+
+;; Lets define a simple data structure for stocks data
+;; This is a vector of maps, as there will be one or more company stocks
+;; to track.  Each map represents the stock information for a company.
+
+(def portfolio [ { :ticker "CRM" :lastTrade 233.12 :open 230.66}
+                 { :ticker "AAPL" :lastTrade 203.25 :open 204.50}
                  { :ticker "MSFT" :lastTrade 29.12  :open 29.08 }
                  { :ticker "ORCL" :lastTrade 21.90  :open 21.83 }])
 
@@ -29,10 +119,36 @@ person
 
 developer-events
 
-dev-event-details
+
 
 ;; (dev-event-details)
 (dev-event-details :devoxxuk)
+
+
+
+(first portfolio)
+(next portfolio)
+
+
+;;; First and next are termed as sequence functions in Clojure,
+;;; unlike other lisps, you can use first and next on other data structures too
+
+(first person)
+(rest person)
+
+;;; these functions return the strings as a set of characters,
+;;; as characters are the elements of a string
+
+;;; returning the first element as a string is easy, simply by rapping the
+;;; str function around the (first person) function
+(str (first person))
+
+;;; So how do we return the rest of the string as a string ?
+(str (rest person))
+(map str (rest person))
+(str (map str (rest person)))
+(apply str (rest person))
+
 
 
 
@@ -48,8 +164,6 @@ dev-event-details
 ;; Set #{}
 ;; a unique set of elements
 
-
-;; Data has behaviour too - homoiconicity
 
 
 (#{:a :b :c} :c)
@@ -71,26 +185,127 @@ dev-event-details
 
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Changing data in data structures
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scope
 
-;; Wait, I thought you said that data structures were immutable!
-;; So how can we change them then?
+;; All def names are publicly available via their namespace.
+;; As def values are immutable, then keeping things private is of less concern than languages built around Object Oriented design.
 
-;; Yes, lists, vectors, maps and sets are all imutable.  However,
-;; you can get a new data structure that has the changes you want.
+;; Private definitions syntax can be used to limit the access to def names to the namespace they are declared in.
 
-(cons 5 '(1 2 3 4))
+;; To limit the scope of a def, add the :private true metadata key value pair.
 
-(cons 5 [1 2 3 4])
+(def ^{:private true} some-var :value)
 
-;; Note what cons has returned...
+;; or
+(def ^:private some-var :value)
 
-(conj '(1 2 3 4) 5)
-
-(conj [1 2 3 4] 5)
-
-;; Conj does what you would expect.
+;; The second form is syntax sugar for the first one.
 
 
+;; You can define a macro for def- 
+(defmacro def- [item value]
+  `(def ^{:private true} ~item ~value)
+)
 
+;; You can then use this macro as follows:
+
+(def- private-definition "This is only accessible in the namespace")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Be Lazy and get more done
+
+; Seqs are an interface for logical lists, which can be lazy.
+; "Lazy" means that a seq can define an infinite series, like so:
+(range 4)
+
+(range) ; => (0 1 2 3 4 ...) (an infinite series)
+
+;; So we dont blow up our memory, just get the values we want
+(take 4 (range)) ;  (0 1 2 3)
+
+;; Clojure (and Lisps in general) tend to evaluate at the last possible moment
+
+; Use cons to add an item to the beginning of a list or vector
+(cons 4 [1 2 3]) ; => (4 1 2 3)
+(cons 4 '(1 2 3)) ; => (4 1 2 3)
+
+; Use conj to add an item to the beginning of a list,
+; or the end of a vector
+(conj [1 2 3] 4) ; => [1 2 3 4]
+(conj '(1 2 3) 4) ; => (4 1 2 3)
+
+; Use concat to add lists or vectors together
+(concat [1 2] '(3 4)) ; => (1 2 3 4)
+
+; Use filter, map to interact with collections
+(map inc [1 2 3]) ; => (2 3 4)
+(filter even? [1 2 3]) ; => (2)
+
+; Use reduce to reduce them
+(reduce + [1 2 3 4])
+; = (+ (+ (+ 1 2) 3) 4)
+; => 10
+
+; Reduce can take an initial-value argument too
+(reduce conj [] '(3 2 1))
+; = (conj (conj (conj [] 3) 2) 1)
+; => [3 2 1]
+
+
+
+;; Playing with data structures
+
+;; Destructuring
+
+(let [[a b c & d :as e] [1 2 3 4 5 6 7]]
+  [a b c d e])
+
+
+(let [[[x1 y1][x2 y2]] [[1 2] [3 4]]]
+  [x1 y1 x2 y2])
+
+;; with strings
+(let [[a b & c :as str] "asdjhhfdas"]
+  [a b c str])
+
+;; with maps
+(let [{a :a, b :b, c :c, :as m :or {a 2 b 3}}  {:a 5 :c 6}]
+  [a b c m])
+
+
+
+
+;; It is often the case that you will want to bind same-named symbols to the map keys. The :keys directive allows you to avoid the redundancy:
+(let [{fred :fred ethel :ethel lucy :lucy} m] )
+
+;; can be written:
+
+(let [{:keys [fred ethel lucy]} m] )
+
+;; As of Clojure 1.6, you can also use prefixed map keys in the map destructuring form:
+
+(let [m {:x/a 1, :y/b 2}
+      {:keys [x/a y/b]} m]
+  (+ a b))
+
+
+; As shown above, in the case of using prefixed keys, the bound symbol name will be the same as the right-hand side of the prefixed key. You can also use auto-resolved keyword forms in the :keys directive:
+
+(let [m {::x 42}
+      {:keys [::x]} m]
+  x)
+
+
+
+;; 4Clojure - exercise 65
+
+(= :map (if (keyword? (first(first {:a 1, :b 2}))) :map))
+
+(if (keyword? (first(first {:a 1 :b 2}))) :map )
+
+(first (first {:a 1 :b 2}))
+
+
+(if (keyword? (first(first %))) :map )
