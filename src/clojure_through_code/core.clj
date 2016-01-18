939fdf378c8fe02c24df0a4dec6baf4d6ae7e015
John Stevenson <john@jr0cket.co.uk>
2016-01-18 00:25:01 +0000

    Added Clojure.test example
    * src/clojure_through_code/core.clj
    
    - Adding a simple function to call four our test example
    
    * test/clojure_through_code/core_test.clj
    
    - Created a simple test with several assertions to show the syntax for
    writing Clojure.test tests.  Included assertions that tests for
    scenarios that should fail.
    
    - fixed the namespace, changing it to clojure-through-code.core

---
+++ src/clojure_through_code/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-through-code.core)
+
+(defn adder
+  "A simple function to add two numbers"
+  [a b]
+  (+ a b))
--- test/clojure_through_code/core_test.clj
+++ test/clojure_through_code/core_test.clj
@@ -1,7 +1,12 @@
 (ns clojure-through-code.core-test
   (:require [clojure.test :refer :all]
-            [devoxxuk.core :refer :all]))
+            [clojure-through-code.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest adder-test
+  (testing "Using a range of numbers to test the adder"
+    #_(is (= 0 1))
+    (is (= (+ 1 2) (adder 1 2)) "Adding 1 and 2")
+    (is (= (+ 1 -2) (adder 1 -2)) "Adding 1 and -2")
+    #_(is (not (= (+ 1 2)) (adder "a" "b")) "Adding strings as negative test")
+    (is (false? (= 0 1)) "A simple failing test")
+    (is (false? (= 0 (adder 3 4))) "Purposefully using failing data")))
