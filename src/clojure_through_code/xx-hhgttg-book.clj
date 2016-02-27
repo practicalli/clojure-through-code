fd8e949e31d866d145168e79dd21cf9fef566796
John Stevenson <john@jr0cket.co.uk>
2016-02-27 14:41:25 +0000

    Expanded description of HHGTTG example
    - Created a clearer description of this example as an introduction
    
    - removed references to LightTable & Instarepl and made it about
    
    - connecting to a REPL
    
    - renamed common-words function to common-english-words
    
    - commented out the functions in the thread-first macro, so that each
    
    - function on the book can be applied in turn and the result shown
    
    - described in comments how the functions work as a general algorithm

--- src/clojure_through_code/xx-hhgttg-book.clj
+++ src/clojure_through_code/xx-hhgttg-book.clj
@@ -1,24 +1,31 @@
-;; Example of threading macros and
-;; the use of LightTable to give fast feedback
-;; as you are developing code
-;; Idea from Misophistful: Understanding thread macros in clojure
+;; Example of threading macros and the use of a connected REPL to give fast feedback
+;; as you are developing code.
+;; Original concept from Misophistful: Understanding thread macros in clojure
 ;; https://www.youtube.com/watch?v=qxE5wDbt964
 
-(str "this is a Lighttable instarepl")
+;; (str "this is live evaluation via a connected repl")
 
-;; Lets play with some data
+;; Lets get the text of a book from a website using the slurp funciton
 
 (def book (slurp "http://clearwhitelight.org/hitch/hhgttg.txt"))
 
-(def common-words
+(def common-english-words
   (-> (slurp "http://www.textfixer.com/resources/common-english-words.txt")
       (clojure.string/split #",")
       set))
 
 (->> book
-     (re-seq #"[a-zA-Z0-9|']+")
-     (map #(clojure.string/lower-case %))
-     (remove common-words)
-     frequencies
-     (sort-by val)
-     reverse)
+     #_(re-seq #"[a-zA-Z0-9|']+")
+     #_(map #(clojure.string/lower-case %))
+     #_(remove common-english-words)
+     #_frequencies
+     #_(sort-by val)
+     #_reverse)
+
+;; Algorithm
+;; * Use regular expression to create a collection of individual words
+;; * Convert all the words to lower case (so they match with common words source)
+;; * Remove the common English words used in the book, leaving more context specific words
+;; * Calculate the requencies of the remaining words, returning a map of word & word count pairs
+;; * Sort the map by word count values
+;; * Reverse the collection so the most commonly used word is the first element in the map
