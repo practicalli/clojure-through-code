435e2489ff229c8a04422287f1fbfed21853badb
John Stevenson <john@jr0cket.co.uk>
2016-03-04 13:45:28 -0800

    Upgrade project to Clojure 1.8
    - updated the org.clojure/clojure dependency from 1.7 to 1.8 now this
    new version has been released

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://jr0cket.co.uk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]])
 
