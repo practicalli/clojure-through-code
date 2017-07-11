2691e0a19ecddf4bb59da825c346e6ceb4a8caea
John Stevenson <john@jr0cket.co.uk>
2017-07-11 17:10:39 +0100

    Solution 68: recur
    Walk through of the problem with the 4clojure solution.

--- src/clojure_through_code/four_clojure.clj
+++ src/clojure_through_code/four_clojure.clj
@@ -60,3 +60,27 @@
                            (cons res (reduct f res (rest se)))))))]
      (lazy-seq (cons firstArg (reduct func firstArg coll))))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; problem 68: recur
+
+
+(loop [x 5
+       result []]
+  (if (> x 0)
+    (recur (dec x) (conj result (+ 2 x)))
+    result))
+
+;; => [7 6 5 4 3]
+
+;; The value of x is bound to 5 and result is bound to an empty vector.
+;; If x is greater than zero, then call the loop again, passing two values
+;; that replace the initial loop bindings.
+;; The conj expression is evaluated before the loop is called again,
+;; so the first value added to result is 7
+;; When the value of x has be decremented to zero, the current value of
+;; result is returned instead of calling the loop again.
+
+;; 4Clojure entered solution
+[7 6 5 4 3]
+
