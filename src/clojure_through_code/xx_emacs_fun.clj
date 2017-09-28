8a545a336e74291976d4a86327496a6095f2ee70
John Stevenson <john@jr0cket.co.uk>
2017-09-28 16:47:56 +0100

    Tidy up examples - only covering snippet use

--- src/clojure_through_code/xx_emacs_fun.clj
+++ src/clojure_through_code/xx_emacs_fun.clj
@@ -1,49 +1,26 @@
 (ns clojure-through-code.xx-emacs-fun
   (:require [clojure.string :as clj-string]))
 
-
-(declare function)
-(declare tab-through-sections)
-(declare blah)
-
 ;; yasnippets
 
 (defn expanded-snippet
-  "This function was created by typing defn and using the binding M-x yas-expand.  I wonder if there is a good shortcut for this, such as using tab"
+  "This function was created by typing defn and using the binding M-x yas-expand.
+  Adding snippets to the autocomplete configuration removes the need to call yas-expand"
   [tab-through-sections]
   (str "I can tab through each of the sections of the snippet to complete it, yay!"))
 
-(def five 5)
 
-(Integer. "80")
-
-(let [x  (+ 2 2)])
+#_(defn meta-forward-slash
+    "Code faster with yasnippets, pressing meta forward-slash to expand the snippet"
+    [tab-through-sections for-fun-and-profit]
+    )
 
 
 ;; Snippets for clojure-mode
-;; bench  defm  deft   for  import  map	       ns    print    test  when
-;; bp     defn  doseq  if	 is	     map.lambda  opts  reduce   try   whenl
-;; def    defr  fn     ifl  let	   mdoc	       pr    require  use
+;; bench  defm  deft   for  import  map        ns    print    test  when
+;; bp     defn  doseq  if  is      map.lambda  opts  reduce   try   whenl
+;; def    defr  fn     ifl  let    mdoc        pr    require  use
 
 
 
-;; require
-;; In an ns function, create a new line and type require, then call `yas-expand'
-
-
-(function  (+ 2 2))
-
-(function  ()() blah)
-
-(+ 1 2 3)
-(+ 1 2)
-1
-
-(defn meta-forward-slash
-  "Code faster with yasnippets, pressing meta forward-slash to expand the snippet"
-  [tab-through-sections for-fun-and-profit]
-  )
-
-
-;FIXME: surprisingly some of my code needs fixing
-
+;; FIXME: surprisingly some of my code needs fixing
