91b4192ccf10bcc5f3b5d0c1d8d4127f03610abc
John Stevenson <practicalli-john@ip-192-168-1-161.eu-central-1.compute.internal>
2022-06-09 13:33:48 +0100

    project: update variable to include aliases
    cider-clojure-cli-aliases is used to include aliases in the REPL startup for
    cider-jack-in
    
    Only alias names are required and are added using the clojure.main -M execution
    option.  Aliases are added before cider/nrepl alias which has its :main-opts
    called to start the repl with extra paths and deps from any other aliases
    specified

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
 ((clojure-mode . ((cider-preferred-build-tool . "clojure-cli")
-                  (cider-clojure-cli-global-options . "-M:lib/java11-source"))))
+                  (cider-clojure-cli-aliases  . ":lib/java17"))))
