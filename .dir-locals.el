97951879415fe606ff295e28e736ab9ab4c2dac5
Johnny Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-04-16 22:23:14 +0100

    emacs config: :lib/reloaded
    Start a REPL process with aliases from practicalli/clojure-cli-config
    - :lib/reloaded - hotload libraries and refresh a namespace

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
 ((clojure-mode . ((cider-preferred-build-tool . "clojure-cli")
-                  (cider-clojure-cli-aliases  . ":lib/java17"))))
+                  (cider-clojure-cli-aliases . ":lib/reloaded"))))
