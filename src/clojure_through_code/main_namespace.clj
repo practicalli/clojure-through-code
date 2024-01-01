d7a87f29358b7dc1f48908b5d52de2ef0b57fda4
Practicalli Engineering <engineering@practical.li>
2024-01-01 00:47:26 +0000

    main: starting clojure

---
+++ src/clojure_through_code/main_namespace.clj
@@ -0,0 +1,10 @@
+(ns clojure-through-code.main-namespace
+  (:require [clojure.string :as string]))
+
+(defn -main
+  "Entry point into the application"
+  [& args]
+  (if (seq? args)
+    (println "The application is running with these arguments:"
+             (string/join " " args))
+    (println "The application is running...")))
---
+++ src/clojure_through_code/parse_cli_options.clj
@@ -0,0 +1,17 @@
+(ns clojure-through-code.parse-cli-options)
+
+
+;; TODO: review the common approach to parsing Clojure CLI command line options
+
+(defn parse-cli-tool-options
+  "Return the merged default options with any provided as a hash-map argument"
+  [arguments]
+  (merge {:replace false :report true :paths ["."]}
+         arguments))
+
+(parse-cli-tool-options {:paths ["src" "test"] :report false})
+;; => {:replace false, :report false, :paths ["src" "test"]}
+
+
+(parse-cli-tool-options {})
+;; => {:replace false, :report true, :paths ["."]}
