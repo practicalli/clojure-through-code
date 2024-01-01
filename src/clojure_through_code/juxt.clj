90b3a3139457d59fe3215a6587585bb35e56fb1b
Practicalli Engineering <engineering@practical.li>
2024-01-01 01:01:30 +0000

    core: juxt

---
+++ src/clojure_through_code/juxt.clj
@@ -0,0 +1,7 @@
+(ns clojure-through-code.juxt)
+
+;; make a map out of some identifiers/values;
+;; this that those -> {:this this :that that :those those}
+
+(into {}
+      (mapv (juxt keyword identity) ['this 'that 'those]))
