6fe11fd539c8b3c64560c53c5ddc0e48563b393f
Practicalli Engineering <engineering@practical.li>
2026-02-10 11:54:50 +0000

    dev: conjure debug output using nrepl 1.4

---
+++ conjure-debug-nrepl14-evaluate-ns-form-1.clj
@@ -0,0 +1,69 @@
+; --------------------------------------------------------------------------------
+; eval (root-form): (ns clojure-through-code.01-basics "Communicate th...
+; debug: send
+{:code
+ "(ns clojure-through-code.01-basics)"
+ :id
+ "d31587d4-3a05-4771-8083-b429b6f8fc30"
+ :nrepl.middleware.print/options
+ {:associative 1 :length 500 :level 50 :right-margin 72}
+ :nrepl.middleware.print/print
+ "cider.nrepl.pprint/pprint"
+ :op
+ "eval"
+ :session
+ "2bd00120-151e-48dd-a416-aa96e3ee0573"}
+; debug: send
+{:code
+ "(ns clojure-through-code.01-basics
+  \"Communicate the purpose of a namespace via a doc-string.
+   Include descriptions of data models defined in the namespace.\")"
+ :column
+ 1
+ :file
+ "/home/practicalli/projects/practicalli/clojure-through-code/src/clojure_through_code/01_basics.clj"
+ :id
+ "0e36c6e8-f696-4557-944e-fe48df58b69c"
+ :line
+ 37
+ :nrepl.middleware.print/options
+ {:associative 1 :length 500 :level 50 :right-margin 72}
+ :nrepl.middleware.print/print
+ "cider.nrepl.pprint/pprint"
+ :ns
+ "clojure-through-code.01-basics"
+ :op
+ "eval"
+ :session
+ "2bd00120-151e-48dd-a416-aa96e3ee0573"}
+; debug: receive
+{:id "d31587d4-3a05-4771-8083-b429b6f8fc30"
+ :ns "clojure-through-code.01-basics"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :value "nil"}
+; debug: receive
+{:id "d31587d4-3a05-4771-8083-b429b6f8fc30"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :status ["done"]}
+; debug: receive
+{:changed-namespaces {}
+ :id "d31587d4-3a05-4771-8083-b429b6f8fc30"
+ :repl-type "clj"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :status ["state"]}
+; debug: receive
+{:id "0e36c6e8-f696-4557-944e-fe48df58b69c"
+ :ns "clojure-through-code.01-basics"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :value "nil"}
+nil
+; debug: receive
+{:id "0e36c6e8-f696-4557-944e-fe48df58b69c"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :status ["done"]}
+; debug: receive
+{:changed-namespaces {}
+ :id "0e36c6e8-f696-4557-944e-fe48df58b69c"
+ :repl-type "clj"
+ :session "2bd00120-151e-48dd-a416-aa96e3ee0573"
+ :status ["state"]}
